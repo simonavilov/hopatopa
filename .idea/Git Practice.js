434035a6607461d2f8e4bd71de9e9bd146fa0045
Simon Avilov <simon.avilov@veeva.com>
2024-09-30 15:03:56 +0100

    commit  number 2

--- .idea/Git Practice.js
+++
@@ -1,2 +0,0 @@
-console.log('Hi, this is the Git Practice file');
-
---
+++ git_practice.js
@@ -0,0 +1,9 @@
+console.log('Hi, this is the Git Practice file');
+
+console.log('Another change');
+
+console.log('Another change again');
+
+console.log('Another 4th change');
+
+edit
